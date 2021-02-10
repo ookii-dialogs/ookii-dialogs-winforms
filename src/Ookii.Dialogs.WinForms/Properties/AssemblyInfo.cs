4c4b9a3afd30ca2481d43428e70d9d5b6c07516a
VPKSoft <vpksoft@vpksoft.net>
2021-02-10 18:00:31 +0200

    Add NET 5 and NET Core 3.1 support.

--- src/Ookii.Dialogs.WinForms/Properties/AssemblyInfo.cs
+++ src/Ookii.Dialogs.WinForms/Properties/AssemblyInfo.cs
@@ -1,23 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Ookii.Dialogs.WinForms")]
-[assembly: AssemblyDescription("")]
-
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Release")]
-#endif
-
-[assembly: AssemblyCompany("augustoproiete.net")]
-[assembly: AssemblyProduct("Ookii.Dialogs.WinForms")]
-[assembly: AssemblyCopyright("Copyright (c) 2009-2021 Ookii Dialogs Contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: System.CLSCompliant(true)]
 [assembly: System.Resources.NeutralResourcesLanguage("en-US")]
 
@@ -28,3 +15,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("16078df2-e2d1-4754-9864-84c288a1e9bd")]
+#endif
